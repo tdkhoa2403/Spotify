bee54c0c63759bdb1e9cdaad8cf6e6d6cb9a5a6d
tdkhoa2403 <tdkhoa213@gmail.com>
2023-10-12 22:15:50 +0700

    second commit

--- js/main.js
+++ js/main.js
@@ -12,6 +12,36 @@ const app = {
                     image: "./imgs/recommendedfortoday/ciaga.jpg",
                     name: "不问ciaga(不问别离)",
                     author: "指尖笑"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/lacchonhongtran.jpg",
+                    name: "Lạc Chốn Hồng Trần Remix (Vinahouse)",
+                    author: "Trà Chanh Remix"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/keobonggon.jpg",
+                    name: "Kẹo Bông Gòn (Remix)",
+                    author: "Cukak, H2K, BHMedia"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/king.jpg",
+                    name: "KING",
+                    author: "Kanaria"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/tgtg.jpg",
+                    name: "Minh Trọng",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/tophit.jpg",
+                    name: "Today's Top Hits",
+                    author: "Doja Cat on top of the Hottest 50!"
+                },
+                {
+                    image: "./imgs/recommendedfortoday/vuongvanimg.jpg",
+                    name: "VƯƠNG VẤN (QINN REMIX)",
+                    author: "Qinn Media, TVk, Hana Cẩm Tiên"
                 }
             ]
         },
@@ -22,6 +52,41 @@ const app = {
                     image: "./imgs/jumpbackin/gigachad.jpg",
                     name: "GigaChad Theme (Phonk House Version)",
                     author: "g3ox_em"
+                },
+                {
+                    image: "./imgs/jumpbackin/tamlongson.jpg",
+                    name: "Tấm Lòng Son",
+                    author: "H-Kray"
+                },
+                {
+                    image: "./imgs/jumpbackin/赐我.jpg",
+                    name: "赐我",
+                    author: "一只白羊"
+                },
+                {
+                    image: "./imgs/jumpbackin/爱是无畏的冒险.jpg",
+                    name: "爱是无畏的冒险",
+                    author: "程今"
+                },
+                {
+                    image: "./imgs/jumpbackin/yoasobi.jpg",
+                    name: "アイドル",
+                    author: "YOASOBI"
+                },
+                {
+                    image: "./imgs/jumpbackin/kawaikutegomen.jpg",
+                    name: "KawaikuteGomen",
+                    author: "TAKANE NO NADESHIKO"
+                },
+                {
+                    image: "./imgs/jumpbackin/メフィスト.jpg",
+                    name: "メフィスト",
+                    author: "QUEEN BEE"
+                },
+                {
+                    image: "./imgs/jumpbackin/missyou.jpg",
+                    name: "Miss you",
+                    author: "Bemax"
                 }
             ]
         },
@@ -29,28 +94,245 @@ const app = {
             id: "recentlyplayed",
             songInfo: [
                 {
-                    image: "./imgs/jumpbackin/gigachad.jpg",
-                    name: "GigaChad Theme (Phonk House Version)",
-                    author: "g3ox_em"
+                    image: "./imgs/recentlyplayed/tiengphaotiennguoi.jpg",
+                    name: "Tiếng Pháo Tiễn Người Remix",
+                    author: "Trà Chanh Remix"
                 },
                 {
-                    image: "./imgs/recommendedfortoday/ciaga.jpg",
-                    name: "不问ciaga(不问别离)",
-                    author: "指尖笑"
+                    image: "./imgs/recentlyplayed/newmedia.jpg",
+                    name: "New Media",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/recentlyplayed/h2oremix.jpg",
+                    name: "H2O Remix",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/recentlyplayed/tiengphaotiennguoi2.jpg",
+                    name: "Tiếng Pháo Tiễn Người Remix",
+                    author: "Trà Chanh Remix"
+                },
+                {
+                    image: "./imgs/recentlyplayed/xinmaruocdau.jpg",
+                    name: "Xin Má Rước Dâu Remix",
+                    author: "Diệu Kiên, Trà Chanh Remix"
+                },
+                {
+                    image: "./imgs/recentlyplayed/trachanhremix.jpg",
+                    name: "Trà Chanh Remix",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/recentlyplayed/thienhanghegi.jpg",
+                    name: "Thiên Hạ Nghe Gì",
+                    author: "Những gì mà người bên cạnh bạn đang nghe. Ảnh bìa: Double2T, Hòa Minzy"
+                },
+                {
+                    image: "./imgs/recentlyplayed/nhukhucnhacchuong.jpg",
+                    name: "Như Khúc Nhạc Chuông",
+                    author: "Nằm cà phê võng lựa nhạc chuông nhạc chờ. Ảnh bìa: Lê Bảo Bình"
+                }
+            ]
+        },
+        {
+            id: "yourtopmixes",
+            songInfo: [
+                {
+                    image: "./imgs/yourtopmixes/meemediamix.jpg",
+                    name: "Mee Media Mix",
+                    author: "YLing, Linh Mino and Anh Quân Idol"
+                },
+                {
+                    image: "./imgs/yourtopmixes/yoasobi.jpg",
+                    name: "YOASOBI Mix",
+                    author: "OFFICIAL HIGE DANDISM, yama and Lilas Ikuta"
+                },
+                {
+                    image: "./imgs/yourtopmixes/zinmediamix.jpg",
+                    name: "ZIN Media Mix",
+                    author: "YLing, Linh Mino and Anh Quân Idol"
+                },
+                {
+                    image: "./imgs/yourtopmixes/moodymix.jpg",
+                    name: "Moody Mix",
+                    author: "Wiz Khalifa, The Walters, Conan Gray and more"
+                },
+                {
+                    image: "./imgs/yourtopmixes/chillmix.jpg",
+                    name: "Chill Mix",
+                    author: "Qinn Media, ZIN Media, Vina Mix and more"
+                },
+                {
+                    image: "./imgs/yourtopmixes/2000smix.jpg",
+                    name: "2000s Mix",
+                    author: "Akon, Jason Mraz, Eminem and more"
+                },
+                {
+                    image: "./imgs/yourtopmixes/90smix.jpg",
+                    name: "90s Mix",
+                    author: "Britney Spears, GRAVECHILL, INTERWORLD and more"
+                },
+                {
+                    image: "./imgs/yourtopmixes/jpopmix.jpg",
+                    name: "J-Pop Mix",
+                    author: "Sangatsu no Phantasia, Kanaria, YOASOBI and more"
+                }
+            ]
+        },
+        {
+            id: "yourfavoriteartists",
+            songInfo: [
+                {
+                    image: "./imgs/yourfavoriteartists/whaledontsleep.jpg",
+                    name: "whaledontsleep",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/yourfavoriteartists/qinnmedia.jpg",
+                    name: "Qinn Media",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/yourfavoriteartists/kanaria.jpg",
+                    name: "Kanaria",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/yourfavoriteartists/kordhell.jpg",
+                    name: "KordHell",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/yourfavoriteartists/mary.jpg",
+                    name: "MARY",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/yourfavoriteartists/sangatsu.jpg",
+                    name: "Sangatsu no Phantasia",
+                    author: "Artist"
+                }
+            ]
+        },
+        {
+            id: "bestofvietnam",
+            songInfo: [
+                {
+                    image: "./imgs/bestofvietnam/topbaihat.jpg",
+                    name: "Top Bài Hát 2022",
+                    author: "Những bài hát được nghe nhiều nhất năm 2022 tại Việt Nam. Ảnh bìa: RPT MCK"
+                },
+                {
+                    image: "./imgs/bestofvietnam/topnhacviet.jpg",
+                    name: "Top Nhạc Việt 2022",
+                    author: "Những nghệ sĩ Việt có lượt nghe nhiều nhất năm 2022. Ảnh bìa: Đen"
+                },
+                {
+                    image: "./imgs/bestofvietnam/bestofhiphop.jpg",
+                    name: "Best Of Hip-hop Việt 2022",
+                    author: "Những ca khúc từ Hip-hop Việt được nghe nhiều nhất năm 2022. Ảnh bìa: tlinh"
+                },
+                {
+                    image: "./imgs/bestofvietnam/topnghesi.jpg",
+                    name: "Top Nghệ Sĩ 2022",
+                    author: "Những nghệ sĩ có lượt nghe nhiều nhất năm 2022 tại Việt Nam. Ảnh bìa: BTS"
+                },
+                {
+                    image: "./imgs/bestofvietnam/bestofthienha.jpg",
+                    name: "Best Of Thiên Hạ Nghe Gì 2022",
+                    author: "Những ca khúc từ Thiên Hạ Nghe Gì được nghe nhiều nhất năm 2022. Ảnh bìa: Vũ."
+                },
+                {
+                    image: "./imgs/bestofvietnam/bestofindie.jpg",
+                    name: "Best Of Indie Việt 2022",
+                    author: "Những ca khúc Indie Việt được nghe nhiều nhất năm 2022. Ảnh bìa: Thịnh Suy"
+                },
+                {
+                    image: "./imgs/bestofvietnam/freshfinds.jpg",
+                    name: "Fresh Finds Vietnam",
+                    author: "Khám phá 🎶 đáng nghe từ những 🎼 có thể bạn chưa biết. Ảnh bìa: MYLINA; thiết kế: Isu Kim"
+                }
+            ]
+        },
+        {
+            id: "uniquelyyours",
+            songInfo: [
+                {
+                    image: "./imgs/uniquelyyours/onrepeat.jpg",
+                    name: "On Repeat",
+                    author: "Songs you love right now"
+                },
+                {
+                    image: "./imgs/uniquelyyours/timecapsule.jpg",
+                    name: "Your Time Capsule",
+                    author: "We made you a personalized playlist with songs to take you back in time."
+                },
+                {
+                    image: "./imgs/uniquelyyours/repeatrewind.jpg",
+                    name: "Repeat Rewind",
+                    author: "Your past favorites"
+                }
+            ]
+        },
+        {
+            id: "popularartists",
+            songInfo: [
+                {
+                    image: "./imgs/popularartists/jungkook.jpg",
+                    name: "Jung Kook",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/rptmck.jpg",
+                    name: "RPT MCK",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/rapviet.jpg",
+                    name: "RAP VIỆT",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/greyd.jpg",
+                    name: "GREY D",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/jimin.jpg",
+                    name: "Jimin",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/v.jpg",
+                    name: "V",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/taylorswift.jpg",
+                    name: "Taylor Swift",
+                    author: "Artist"
+                },
+                {
+                    image: "./imgs/popularartists/latto.jpg",
+                    name: "Latto",
+                    author: "Artist"
                 }
             ]
         }
     ],
     render() {
-        const listItem = document.querySelectorAll(".list-item");
-        listItem.forEach(item => {
+        const listItems = document.querySelectorAll(".list-item");
+        listItems.forEach(listItem => {
             this.listSong.forEach(song => {
-                if(item.id == song.id) {
+                if(listItem.id == song.id) {
                     for(let i = 0; i < song.songInfo.length; i++) {
-                        item.innerHTML += `
+                        listItem.innerHTML += `
                             <div class="item">
                                 <div class="col song-card">
-                                    <img src="${song.songInfo[i].image}" alt="GigaChad">
+                                    <div class="itemImg">
+                                        <img src="${song.songInfo[i].image}" alt="GigaChad">
+                                    </div>
                                     <div class="desc">
                                         <h3 class="name">${song.songInfo[i].name}</h3>
                                         <p class="author">${song.songInfo[i].author}</p>
@@ -68,7 +350,26 @@ const app = {
                     }
                 }
             })
-        })
+            const items = listItem.querySelectorAll(".item");
+            items.forEach(item => {
+                const imgSong = item.querySelector("img");
+                const author = item.querySelector(".author");
+                if(author.innerHTML == "Artist") {
+                    imgSong.classList.add("imgSong");
+                    author.classList.add("mgb-22");
+                }
+            });
+            // window.addEventListener('resize', function(event){
+            //     if(this.window.innerWidth <= 1682) {
+            //         items.forEach(item => {
+            //             const lastItem = items[items.length - 1];
+            //             if(item.offsetWidth == 169) {
+            //                 lastItem.style.display = "none";
+            //             }
+            //         });
+            //     }
+            // });                       
+        });
     },
     start() {
         this.render();
